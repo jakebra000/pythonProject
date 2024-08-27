7a841da9f2f0b151b43d2451f1d4c6c574b9b628
Brayden jake <jakebra000@warren.k12.in.us>
2024-08-27 10:11:28 -0400

    done

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input("What is your name:Brayden ")
+#INSTRUCTION:  write an input
+# statement that asks for
+# number of droid and store as droids
+#INSTRUCTION:  write an input
+# statement that asks for
+# number of wookies and store as wookiees
+print(brayden, "wants to meet",3 droids, "droids and 2, wookiees, "Wookiees!")
